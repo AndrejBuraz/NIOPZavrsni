7eed04e6118e143a3f28201534610897f1216f92
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-05-24 09:28:20 +0200

    Add files via upload

---
+++ NIOPZavrsniProjekt/NIOPZavrsniProjekt/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NIOPZavrsniProjekt
+{
+    public partial class FormUnosFilma : Form
+    {
+        public FormUnosFilma()
+        {
+            InitializeComponent();
+        }
+    }
+}
